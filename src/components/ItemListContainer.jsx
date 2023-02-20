275adb23652ba7ce3ccfbc1617249a5ee41d2ba3
Nicolas Nazarre <demillamotos@yahoo.com.ar>
2023-02-19 21:07:52 -0300

    preparando el TP2

--- src/components/ItemListContainer.jsx
+++ src/components/ItemListContainer.jsx
@@ -1,12 +1,27 @@
+import { useEffect } from "react";
+import ItemCount from "./ItemCount";
+import ItemList from "./ItemList";
+import arrayProductos from "./json/productos.json"
+
 const ItemListContainer = ({greeting}) => {
+    const[items, setItems] = useState([]);
+
+    useEffect(() => {
+            const promesa = new Promise((resolve) => {
+                setTimeout(() => {
+                    resolve(arrayProductos);
+                }, 2000);
+            });
+            promesa.then((respuesta) => {
+                setItems(respuesta);
+            });
+    }, []);
+    
     return (
         <div className="container my-5">
             <div className="row">
-                <div className="col text-center">
-                    <div className="alert alert-danger" role="alert">
-                        <p>{greeting}</p>
-                    </div>
-                </div>
+                <ItemList items={items} />
+                <ItemCount stock={10} />
             </div>
         </div>
     )
